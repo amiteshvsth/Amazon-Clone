aaa83dd7010ea42682998ed31d541def5a17506a
Amitesh <amiteshvashishth@gmail.com>
2023-09-16 13:01:38 +0530

    Added add to cart funct and installed curr format

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -3,17 +3,17 @@ import './App.css';
 import Header from './Header';
 import Home from './Home';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Checkout from './Checkout';
 
 function App() {
   return (
     //BEM Convention
     <Router>
+      <Header />
       <div className="app">
         <Routes>
-          <Route path='/'>
-            <Header />
-            <Home />
-          </Route>
+          <Route path='/' element={<Home />} />
+          <Route path='/checkout' element={<Checkout />} />
         </Routes>
       </div>
     </Router>
--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -2,12 +2,15 @@ import React from 'react'
 import './Header.css'
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <div className="header">
+        <Link to='/'>
         <img alt=''
-        className="header_logo"src="https://www.amazon.com/ref=nav_logo" 
+        className="header_logo"src="https://www.thesun.co.uk/wp-content/uploads/2022/02/Amazon-Logo-1024x426-1.png" 
         />
+        </Link>
         <div className="header_search">
             <input type="text" className="header_searchInput" />
             <SearchIcon className='header_searchIcon'/>
@@ -27,10 +30,12 @@ const Header = () => {
                 <span className="header_option1">Your</span>
                 <span className="header_option2">Prime</span>
             </div>
+            <Link to='/checkout'>
             <div className="header_optionsBasket">
                 <ShoppingBasketIcon/>
                 <span className="header_option1 header_basketCount">0</span>
             </div>
+            </Link>
         </div>
     </div>
   )
