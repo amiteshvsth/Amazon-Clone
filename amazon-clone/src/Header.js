6306ee0f47fa52a83f637f37774923846cd7a4fd
Amitesh <amiteshvashishth@gmail.com>
2023-10-21 16:20:29 +0530

    Getting total items using useStateValue

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -3,7 +3,9 @@ import './Header.css'
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import { Link } from 'react-router-dom';
+import { useStateValue } from './StateProvider';
 const Header = () => {
+    const [{basket},dispatch]=useStateValue();
   return (
     <div className="header">
         <Link to='/'>
@@ -33,11 +35,11 @@ const Header = () => {
             <Link to='/checkout'>
             <div className="header_optionsBasket">
                 <ShoppingBasketIcon/>
-                <span className="header_option1 header_basketCount">0</span>
+                <span className="header_option1 header_basketCount">{basket?.length}</span>
             </div>
             </Link>
         </div>
     </div>
-  )
+  );
 }
-export default Header
+export default Header;
