a5bd414145b35fae7aa81b854fa72a193da03bde
Amitesh <amiteshvashishth@gmail.com>
2023-10-21 16:24:57 +0530

    Added original bakset using useState

--- amazon-clone/src/Checkout.js
+++ amazon-clone/src/Checkout.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import './Checkout.css'
 import Subtotal from './Subtotal';
+import CheckoutProduct from './CheckoutProduct';
+import { useStateValue } from './StateProvider';
 
 const Checkout = () => {
+  const [{basket},dispatch]= useStateValue();
   return (
     <div className='checkout'>
       <div className="checkout_left">
         <img src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="" className="checkout_ad" />
         <div>
           <h2 className="checkout_title">Your Shopping Basket</h2>
-          {/* Basket title */}
-          {/* Basket title */}
-          {/* Basket title */}
-          {/* Basket title */}
-          {/* Basket title */}
+
+          {basket.map(item => (
+            <CheckoutProduct
+            id={item.id}
+            title={item.title}
+            image={item.image}
+            price={item.price}
+            rating={item.rating}
+            />
+          ))}
 
         </div>
       </div>
